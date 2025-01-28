cb7a8793b350321fb729ea8cf56ffb56b838e826
Chibundu <ndubest56@gmail.com>
2025-01-28 16:54:46 +0100

    more on scripts

--- GCLASS/app.js
+++ GCLASS/app.js
@@ -14,5 +14,37 @@ changer()
 // function handleChange() {
 //   document.getElementById("scope").innerHTML = myItems;
 // }
+// message()
 
-message()
+let myArray = [
+  'Hello World!',
+  'This example demonstra method',
+  'Hello Worldssssss!.',
+  'This example demonstra methodssssssss.',
+]
+
+console.log(myArray[0])
+
+function validateForm() {
+  let first = document.forms['form']['fname'].value
+  let last = document.forms['form']['lname'].value
+  let email = document.forms['form']['email'].value
+
+  if (first == '') {
+    alert('First Name must be filled out')
+    return false
+  } else if (last == '') {
+    alert('Last Name must be filled out')
+    return false
+  } else if (email == '') {
+    alert('Email must be filled out')
+    return false
+  } else {
+    return true
+  }
+}
+
+function typing() {
+  let x = document.getElementById('email').value
+  document.getElementById('demo').innerHTML = x
+}
